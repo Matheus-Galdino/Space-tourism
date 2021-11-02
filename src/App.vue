fdc46cfc1cd86019e90da1cb0fe62d55a4305202
Matheus Carvalho <matheus10277@gmail.com>
2021-11-01 22:36:23 -0300

    Criando componente Nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
+  <Nav />
   <router-view />
 </template>
+
+<script>
+import Nav from "./components/Nav.vue";
+
+export default {
+  name: "App",
+  components: { Nav },
+};
+</script>
 
 <style lang="scss">
 @import url("./styles/reset.scss");
 @import url("./styles/variables.scss");
+
+#app {
+  height: 100vh;
+  padding: 2.4rem 2.4rem 5rem;
+
+  background-size: cover;
+  background: url("./assets/home/background-home-tablet.jpg") no-repeat;
+}
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <img src="../assets/shared/logo.svg" alt="logo" />
+    <button class="burger">
+      <div class="line line-1"></div>
+      <div class="line line-2"></div>
+      <div class="line line-3"></div>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+
+<style lang="scss">
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 2.5rem;
+}
+
+.burger {
+  background: none;
+}
+
+.line {
+  width: 24px;
+  height: 3px;
+  background: #fff;
+
+  &:not(:last-of-type) {
+    margin-bottom: 5px;
+  }
+}
+</style>
