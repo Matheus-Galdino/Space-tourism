0ff12ec1177f95ff1e8fec2051e9e31d5081e888
Matheus Carvalho <matheus10277@gmail.com>
2021-11-05 11:18:46 -0300

    Criando componente de navegação da crew page

---
+++ src/components/CrewNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="crew-nav">
+    <router-link class="ellipse" to="/crew/douglas"></router-link>
+    <router-link class="ellipse" to="/crew/mark"></router-link>
+    <router-link class="ellipse" to="/crew/victor"></router-link>
+    <router-link class="ellipse" to="/crew/anousheh"></router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "CrewNav",
+};
+</script>
+
+<style lang="scss" scoped>
+.crew-nav {
+  display: flex;
+  column-gap: 1.6rem;
+  align-items: center;
+}
+
+.ellipse {
+  width: 1rem;
+  height: 1rem;
+  opacity: 0.17;
+  border-radius: 50%;
+  background: #fff;
+}
+
+.router-link-exact-active {
+  opacity: 1;
+}
+</style>
