8a2f77a794d80dfb3396edc662a6095b0474f507
Matheus Carvalho <matheus10277@gmail.com>
2021-11-08 15:44:51 -0300

    Implementando layout mobile tech page

--- src/views/Tech.vue
+++ src/views/Tech.vue
@@ -1,20 +1,91 @@
 <template>
-  <section class="page launch">
+  <section class="page tech">
     <p class="page__id">
       <span>03</span>
       Space launch 101
+    </p>
+
+    <img :src="techImg" :alt="currentTech.name" loading="lazy" class="tech__img" />
+
+    <TechNav v-model="currentIndex" class="tech-nav" />
+
+    <h2>the terminology...</h2>
+    <h1 class="name">{{ currentTech.name }}</h1>
+
+    <p class="description">
+      {{ currentTech.description }}
     </p>
   </section>
 </template>
 
 <script>
+import TechNav from "../components/TechNav.vue";
+
 export default {
   name: "Tech",
+  components: { TechNav },
+  data() {
+    return {
+      techs: [],
+      currentIndex: 1,
+    };
+  },
+  computed: {
+    currentTech() {
+      return this.techs[this.currentIndex - 1];
+    },
+    techImg() {
+      return require(`../assets/${this.currentTech.images.landscape}`);
+    },
+  },
+  beforeMount() {
+    const info = require("../data.json");
+    this.techs = info.technology;
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.launch {
+.tech {
   --backgroundImg: url("../assets/technology/background-technology-mobile.jpg");
+
+  padding-inline: 0;
+}
+
+.tech__img {
+  width: 100%;
+  height: 17rem;
+  object-fit: cover;
+}
+
+.tech-nav {
+  margin: 3rem 0;
+}
+
+h2 {
+  font-size: 1.4rem;
+  line-height: 1.2em;
+  letter-spacing: 2px;
+  font-weight: normal;
+
+  color: #d0d6f9;
+  text-transform: uppercase;
+}
+
+.name {
+  font-size: 2.4rem;
+  line-height: 1.2em;
+  font-weight: normal;
+  margin: 0.8rem 0 1.6rem;
+  text-transform: uppercase;
+}
+
+.description {
+  font-size: 1.5rem;
+  line-height: 1.7em;
+
+  color: #d0d6f9;
+  padding: 0 2.5rem;
+  text-align: center;
 }
 </style>
